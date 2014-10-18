f5aace44ec4ee8e84b9a8688b23b25ba8d3851d2
nieup <nieup17@gmail.com>
2014-10-18 16:22:09 +0200

    First commit to V0
    Les premiers fichiers du projet, vide mais commentés :)

---
+++ mainClient.c
@@ -0,0 +1,30 @@
+/**********************************************************
+*
+*	File : mainServeur.c
+*	Author : Cédric HUMBERT
+*
+*	Description :
+*	Ce fichier contient le code du programe principale coté
+*   seveur du system TOMM
+*
+*
+*
+*	Date : 18/ 10/ 2014
+*
+*************************************************************/
+
+#include <stdio.h>
+#include <stdin.h>
+
+// Fichiers Headers de structures :
+
+// Fichiers Headers des fonctions annexes :
+
+void main(){
+
+
+
+
+
+}
+
